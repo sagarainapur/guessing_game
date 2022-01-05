4cfa2d922819e986c817cc6a1737828dfc5ecb9e
Sagar Ainapur <46922060+sagarainapur@users.noreply.github.com>
2022-01-05 19:57:09 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,15 +1,19 @@
+#Guessing game
 echo How many files are present in the current directory
 
+#Enter the guessing number
 read guess
 
+#Calculate the number of files in directories 
 count=ls -l | wc -l
 
+#if else statement
 if [ $count -eq $guess ]
 then
-	exit
+	exit	
 elif [ $count -gt $guess ]
 then
-	echo guess was too high
+	echo guess was too high	
 elif [ $count -lt $guess ]
 then
 	echo guess was too low
