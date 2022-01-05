6e2d9d6774779336d9049b0fb01d22d11d5c99dd
Sagar Ainapur <46922060+sagarainapur@users.noreply.github.com>
2022-01-05 19:51:57 +0530

    adding files_v1

---
+++ guessinggame.sh
@@ -0,0 +1,16 @@
+echo How many files are present in the current directory
+
+read guess
+
+count=ls -l  wc -l
+
+if [ $count -eq $guess ]
+then
+	exit
+elif [ $count -gt $guess ]
+then
+	echo guess was too high
+elif [ $count -lt $guess ]
+then
+	echo guess was too low
+fi
