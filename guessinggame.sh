8fcd42e0e441622e0a0038b9a47a8a1b45ea5869
Sagar Ainapur <46922060+sagarainapur@users.noreply.github.com>
2022-01-10 15:31:40 +0530

    v3
    type mistakes, nothing else

--- guessinggame.sh
+++ guessinggame.sh
@@ -7,17 +7,6 @@ read guess
 #Calculate the number of files in directories 
 count=ls -l | wc -l
 
-#if else statement
-if [ $count -eq $guess ]
-then
-	exit	
-elif [ $count -gt $guess ]
-then
-	echo guess was too high	
-elif [ $count -lt $guess ]
-then
-	echo guess was too low
-fi
 
 #while statement
 while ![ $count -eq $guess ]
